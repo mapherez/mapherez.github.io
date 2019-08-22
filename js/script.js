fd5974993fcff38eea40ce623a1ea7d2faced0d3
mapherez <mapherez@users.noreply.github.com>
2019-08-22 23:49:20 +0100

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/*$(function() {
+    $( "#sortable" ).sortable({
+        axis: "y",
+        cancel: "li.non-selectable"
+    });
+    $( "#draggable" ).draggable({
+        connectToSortable: "#sortable",
+        containment: "#table-wrap",
+        axis: "y"                
+    });
+    $( "ul, li" ).disableSelection();
+});*/
